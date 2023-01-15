636b10e474537e9aa9e8a4c18bbf26cff8e154ec
andreypoltev <andreypoltev@gmail.com>
2023-01-15 16:42:14 +0700

    Now it adds country phone code to the bank phone number based on "alpha2" value.

--- app/src/main/java/com/poltevab/binchecker/MainActivity.kt
+++ app/src/main/java/com/poltevab/binchecker/MainActivity.kt
@@ -62,9 +62,8 @@ class MainActivity : AppCompatActivity() {
                 CoroutineScope(IO).launch {
                     val cardNumber = binding.editText.text.toString().toInt()
                     val output = getOutput(cardNumber)
-                    if (output != null) {
+                    if (output != null)
                         setTextViewValues(output)
-                    }
                     refreshAdapter(listViewRecentHistory, adapterRecentHistory)
                 }
             } else  {
@@ -141,9 +140,10 @@ class MainActivity : AppCompatActivity() {
         val url = URL("https://lookup.binlist.net/$cardNumber")
         var fetchedData: String
         val connection: HttpURLConnection
-            withContext(IO) {
-                connection = url.openConnection() as HttpURLConnection
-            }
+
+        withContext(IO) {
+            connection = url.openConnection() as HttpURLConnection
+        }
 
         when (connection.responseCode) {
             200 -> {
@@ -182,25 +182,94 @@ class MainActivity : AppCompatActivity() {
     suspend fun setTextViewValues(parsedData: ParsedData): String {
 
         withContext(Main) {
-            binding.textViewNumberLength.text = parsedData.numberLength.toString()
-            binding.textViewNumberLuhn.text = parsedData.numberLuhn.toString()
 
-            binding.textViewScheme.text = parsedData.scheme
-            binding.textViewType.text = parsedData.type
-            binding.textViewBrand.text = parsedData.brand
-            binding.textViewPrepaid.text = parsedData.prepaid.toString()
+            if (parsedData.numberLength.toString() == "null")
+                binding.textViewNumberLength.text = "NO DATA"
+            else
+                 binding.textViewNumberLength.text = parsedData.numberLength.toString()
+
+
+
+            if (parsedData.numberLuhn.toString() == "null")
+                binding.textViewNumberLuhn.text = "NO DATA"
+            else if (parsedData.numberLuhn.toString() == "true")
+                binding.textViewNumberLuhn.text = "YES"
+            else
+                binding.textViewNumberLuhn.text = "NO"
+
+
+
+            if (parsedData.scheme.toString() == "null")
+                binding.textViewScheme.text = "NO DATA"
+            else
+                binding.textViewScheme.text = parsedData.scheme
+
+
+
+            if (parsedData.type.toString() == "null")
+                binding.textViewType.text = "NO DATA"
+            else
+                binding.textViewType.text = parsedData.type
+
+
+
+            if (parsedData.brand.toString() == "null")
+                binding.textViewBrand.text = "NO DATA"
+            else
+                binding.textViewBrand.text = parsedData.brand
+
+
+
+            if (parsedData.prepaid.toString() == "null")
+                binding.textViewPrepaid.text = "NO DATA"
+            else if (parsedData.prepaid.toString() == "true")
+                binding.textViewPrepaid.text = "YES"
+            else
+                binding.textViewPrepaid.text = "NO"
 
             binding.textViewCountryNumeric.text = parsedData.countryNumeric
             binding.textViewCountryAlpha2.text = parsedData.countryAlpha2
-            binding.textViewCountryName.text = parsedData.countryName
+
+            if (parsedData.countryName.toString() == "null")
+                binding.textViewCountryName.text = "NO DATA"
+            else
+                binding.textViewCountryName.text = parsedData.countryName
+
             binding.textViewCountryEmoji.text = parsedData.countryEmoji
             binding.textViewCountryCurrency.text = parsedData.countryCurrency
-            binding.textViewCountryLatitude.text = parsedData.countryLatitude.toString()
-            binding.textViewCountryLongitude.text = parsedData.countryLongitude.toString()
 
-            binding.textViewBankName.text = parsedData.bankName
+            if (parsedData.countryLatitude.toString() == "null")
+                binding.textViewCountryLatitude.text = "NO DATA"
+            else
+                binding.textViewCountryLatitude.text = parsedData.countryLatitude.toString()
+
+            if (parsedData.countryLongitude.toString() == "null")
+                binding.textViewCountryLongitude.text = "NO DATA"
+            else
+                binding.textViewCountryLongitude.text = parsedData.countryLongitude.toString()
+
+
+
+            if (parsedData.bankName.toString() == "null")
+                binding.textViewBankName.text = "NO DATA"
+            else
+                binding.textViewBankName.text = parsedData.bankName
+
+
+
+            if (parsedData.bankPhone.toString() == "null" && !parsedData.bankPhone.toString().startsWith("+")) {
+                binding.textViewBankPhone.text = ""
+            } else if (parsedData.bankPhone.toString().startsWith("+")){
+                binding.textViewBankPhone.text = parsedData.bankPhone
+            } else {
+                val countryPhoneCode = getCountryPhoneCode(parsedData.countryAlpha2.toString())
+                val output = "$countryPhoneCode ${parsedData.bankPhone}"
+                binding.textViewBankPhone.text = output
+            }
+
+
+
             binding.textViewBankUrl.text = parsedData.bankUrl
-            binding.textViewBankPhone.text = parsedData.bankPhone
             binding.textViewBankCity.text = parsedData.bankCity
 
           //  binding.textView.text = getOutputAsArrayList(parsedData).toString()
@@ -264,6 +333,253 @@ class MainActivity : AppCompatActivity() {
             jsonObject.optJSONObject("bank")?.optString("phone"),
             jsonObject.optJSONObject("bank")?.optString("city")
         )
+    }
+
+    fun getCountryPhoneCode(countryCode: String): String {
+        val countryCodes = mapOf<String, String>(
+            "AF" to "+93",
+            "AL" to "+355",
+            "DZ" to "+213",
+            "AS" to "+1-684",
+            "AD" to "+376",
+            "AO" to "+244",
+            "AI" to "+1-264",
+            "AQ" to "+672",
+            "AG" to "+1-268",
+            "AR" to "+54",
+            "AM" to "+374",
+            "AW" to "+297",
+            "AU" to "+61",
+            "AT" to "+43",
+            "AZ" to "+994",
+            "BS" to "+1-242",
+            "BH" to "+973",
+            "BD" to "+880",
+            "BB" to "+1-246",
+            "BY" to "+375",
+            "BE" to "+32",
+            "BZ" to "+501",
+            "BJ" to "+229",
+            "BM" to "+1-441",
+            "BT" to "+975",
+            "BO" to "+591",
+            "BA" to "+387",
+            "BW" to "+267",
+            "BR" to "+55",
+            "IO" to "+246",
+            "VG" to "+1-284",
+            "BN" to "+673",
+            "BG" to "+359",
+            "BF" to "+226",
+            "BI" to "+257",
+            "KH" to "+855",
+            "CM" to "+237",
+            "CA" to "+1",
+            "CV" to "+238",
+            "KY" to "+1-345",
+            "CF" to "+236",
+            "TD" to "+235",
+            "CL" to "+56",
+            "CN" to "+86",
+            "CX" to "+61",
+            "CC" to "+61",
+            "CO" to "+57",
+            "KM" to "+269",
+            "CK" to "+682",
+            "CR" to "+506",
+            "HR" to "+385",
+            "CU" to "+53",
+            "CW" to "+599",
+            "CY" to "+357",
+            "CZ" to "+420",
+            "CD" to "+243",
+            "DK" to "+45",
+            "DJ" to "+253",
+            "DM" to "+1-767",
+            "DO" to "+1-809",
+            "TL" to "+670",
+            "EC" to "+593",
+            "EG" to "+20",
+            "SV" to "+503",
+            "GQ" to "+240",
+            "ER" to "+291",
+            "EE" to "+372",
+            "ET" to "+251",
+            "FK" to "+500",
+            "FO" to "+298",
+            "FJ" to "+679",
+            "FI" to "+358",
+            "FR" to "+33",
+            "PF" to "+689",
+            "GA" to "+241",
+            "GM" to "+220",
+            "GE" to "+995",
+            "DE" to "+49",
+            "GH" to "+233",
+            "GI" to "+350",
+            "GR" to "+30",
+            "GL" to "+299",
+            "GD" to "+1-473",
+            "GU" to "+1-671",
+            "GT" to "+502",
+            "GG" to "+44-1481",
+            "GN" to "+224",
+            "GW" to "+245",
+            "GY" to "+592",
+            "HT" to "+509",
+            "HN" to "+504",
+            "HK" to "+852",
+            "HU" to "+36",
+            "IS" to "+354",
+            "IN" to "+91",
+            "ID" to "+62",
+            "IR" to "+98",
+            "IQ" to "+964",
+            "IE" to "+353",
+            "IM" to "+44-1624",
+            "IL" to "+972",
+            "IT" to "+39",
+            "CI" to "+225",
+            "JM" to "+1-876",
+            "JP" to "+81",
+            "JE" to "+44-1534",
+            "JO" to "+962",
+            "KZ" to "+7",
+            "KE" to "+254",
+            "KI" to "+686",
+            "XK" to "+383",
+            "KW" to "+965",
+            "KG" to "+996",
+            "LA" to "+856",
+            "LV" to "+371",
+            "LB" to "+961",
+            "LS" to "+266",
+            "LR" to "+231",
+            "LY" to "+218",
+            "LI" to "+423",
+            "LT" to "+370",
+            "LU" to "+352",
+            "MO" to "+853",
+            "MK" to "+389",
+            "MG" to "+261",
+            "MW" to "+265",
+            "MY" to "+60",
+            "MV" to "+960",
+            "ML" to "+223",
+            "MT" to "+356",
+            "MH" to "+692",
+            "MR" to "+222",
+            "MU" to "+230",
+            "YT" to "+262",
+            "MX" to "+52",
+            "FM" to "+691",
+            "MD" to "+373",
+            "MC" to "+377",
+            "MN" to "+976",
+            "ME" to "+382",
+            "MS" to "+1-664",
+            "MA" to "+212",
+            "MZ" to "+258",
+            "MM" to "+95",
+            "NA" to "+264",
+            "NR" to "+674",
+            "NP" to "+977",
+            "NL" to "+31",
+            "AN" to "+599",
+            "NC" to "+687",
+            "NZ" to "+64",
+            "NI" to "+505",
+            "NE" to "+227",
+            "NG" to "+234",
+            "NU" to "+683",
+            "KP" to "+850",
+            "MP" to "+1-670",
+            "NO" to "+47",
+            "OM" to "+968",
+            "PK" to "+92",
+            "PW" to "+680",
+            "PS" to "+970",
+            "PA" to "+507",
+            "PG" to "+675",
+            "PY" to "+595",
+            "PE" to "+51",
+            "PH" to "+63",
+            "PN" to "+64",
+            "PL" to "+48",
+            "PT" to "+351",
+            "PR" to "+1-787",
+            "QA" to "+974",
+            "CG" to "+242",
+            "RE" to "+262",
+            "RO" to "+40",
+            "RU" to "+7",
+            "RW" to "+250",
+            "BL" to "+590",
+            "SH" to "+290",
+            "KN" to "+1-869",
+            "LC" to "+1-758",
+            "MF" to "+590",
+            "PM" to "+508",
+            "VC" to "+1-784",
+            "WS" to "+685",
+            "SM" to "+378",
+            "ST" to "+239",
+            "SA" to "+966",
+            "SN" to "+221",
+            "RS" to "+381",
+            "SC" to "+248",
+            "SL" to "+232",
+            "SG" to "+65",
+            "SX" to "+1-721",
+            "SK" to "+421",
+            "SI" to "+386",
+            "SB" to "+677",
+            "SO" to "+252",
+            "ZA" to "+27",
+            "KR" to "+82",
+            "SS" to "+211",
+            "ES" to "+34",
+            "LK" to "+94",
+            "SD" to "+249",
+            "SR" to "+597",
+            "SJ" to "+47",
+            "SZ" to "+268",
+            "SE" to "+46",
+            "CH" to "+41",
+            "SY" to "+963",
+            "TW" to "+886",
+            "TJ" to "+992",
+            "TZ" to "+255",
+            "TH" to "+66",
+            "TG" to "+228",
+            "TK" to "+690",
+            "TO" to "+676",
+            "TT" to "+1-868",
+            "TN" to "+216",
+            "TR" to "+90",
+            "TM" to "+993",
+            "TC" to "+1-649",
+            "TV" to "+688",
+            "VI" to "+1-340",
+            "UG" to "+256",
+            "UA" to "+380",
+            "AE" to "+971",
+            "GB" to "+44",
+            "US" to "+1",
+            "UY" to "+598",
+            "UZ" to "+998",
+            "VU" to "+678",
+            "VA" to "+379",
+            "VE" to "+58",
+            "VN" to "+84",
+            "WF" to "+681",
+            "EH" to "+212",
+            "YE" to "+967",
+            "ZM" to "+260",
+            "ZW" to "+263"
+        )
+
+        return countryCodes[countryCode].toString()
     }
 
     fun saveData() {
